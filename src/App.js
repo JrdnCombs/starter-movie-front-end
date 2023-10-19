98ae0e6543288414db8be03b521c932e8ac2a128
Jordan Combs <jrdncombs@gmail.com>
2023-10-19 06:49:20 -0500

    adding fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React, { useState, useEffect } from 'react'
+
 import Header from "./shared/Header";
 import MoviesList from "./home/MoviesList";
 import DetailedMoviesList from "./movies/DetailedMoviesList";
 import FullMovie from "./movie/FullMovie";
 import TheaterList from "./theaters/TheaterList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+useEffect(() => {
+  fetch('https://we-love-movies-uavc.onrender.com')
+    .then((res) => res.json())
+    .then((name) => setName([name]))
+
 
 function App() {
   return (
